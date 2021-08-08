ac8b287a2c3708b480322928e468427d10b5c69d
tieugia <buicherry01@gmail.com>
2021-08-08 15:34:02 +0700

    update

---
+++ Demo/Fullstack.Net/Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Asp.netCoreMVCCRUD.Models;
+
+namespace Asp.netCoreMVCCRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+
+        // GET: User/Create
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new User());
+            else
+                return View(_context.Users.Find(id));
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("UserId,FullName,PhoneNumber,Position,")] User User)
+        {
+            if (ModelState.IsValid)
+            {
+                if (User.UserId == 0)
+                    _context.Add(User);
+                else
+                    _context.Update(User);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(User);
+        }
+
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var User =await _context.Users.FindAsync(id);
+            _context.Users.Remove(User);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ Demo/Fullstack.Net/Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="This field is required.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+    }
+}
